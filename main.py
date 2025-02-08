ff6a2f0c4d7ad3f1fda89deb5519128abff2c866
Colleen Que <quecolleen@gmail.com>
2025-02-08 16:26:47 -0500

    gui result

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import os
 from simple_draw import PhantomPen
 import argparse
+import tkinter as tk
+from simple_gui import SimpleGUI
+
+
+def start_gui(is_authenticated):
+    """Start the GUI application."""
+    window = tk.Tk()
+    app = SimpleGUI(window, is_authenticated)
+    window.mainloop()
+
 
 if __name__ == "__main__":
+   
+    # from simple draw
     parser = argparse.ArgumentParser(description="Simple draw & signature collection app")
     parser.add_argument("-n", "--name", type=str, default="user", help="the name of the user")
     parser.add_argument("-s", "--signature_dir", type=str, default="test", help="Directory to store signatures")
@@ -10,6 +22,7 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--phantom", action="store_true", help="Enable phantom effect")
     # Add necessary arguments
     # TODO
+    # arguments
     parser.add_argument("--ckpt_path", type=str, default=None, help="Path to the trained model checkpoint (e.g., siamese_signature_model.pth)")
     parser.add_argument("--real_dir", type=str, default="signatures/rickyy", help="Path to the real signature directory")
     parser.add_argument("--fake_dir", type=str, default="signatures/ricky", help="Path to the fake signature directory")
@@ -38,4 +51,6 @@ if __name__ == "__main__":
 
     # Pop-up login success or failure message (GUI)
     # TODO
+    authenticated = True 
+    start_gui(authenticated)
     
